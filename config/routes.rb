a568b5c7a81b992b2987649855b4371978b88643
Andrey Shimanovich <strikebgu84@mail.ru>
2015-02-07 17:49:38 +0300

    Половину спеков не работает. Ну что поделать нужно двигаться дальше. Закоменчу все да задеплою. Перед сливанием веток.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,18 @@
 SampleApp::Application.routes.draw do
+  get 'users/new'
+
   #root :to => "controller#action"
   root  'static_pages#home'
-  match '/help',    to: 'static_pages#help',    via: 'get'
-  match '/about',   to: 'static_pages#about',   via: 'get'
-  match '/contact', to: 'static_pages#contact', via: 'get'
+  match '/signup',   to: 'users#new',                   via: 'get'
+  match '/help',       to: 'static_pages#help',       via: 'get'
+  match '/about',     to: 'static_pages#about',     via: 'get'
+  match '/contact',  to: 'static_pages#contact',  via: 'get'
 
 
 #Rails.application.routes.draw do
+
+# get 'users/new'
+
 #  root :to => 'static_pages#home'
 #  match '/help',    to: 'static_pages#help',    via: 'get'
 #  match '/about',   to: 'static_pages#about',   via: 'get'
